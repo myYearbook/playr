30095ad155cd4d2101ee81400626718563beebc2
Jonah H. Harris <jonah.harris@gmail.com>
2008-07-25 11:44:17 +0000

    Workaround for memory corruption issue

--- src/avltree.c
+++ src/avltree.c
@@ -127,7 +127,8 @@ int avl_rotate(AVL_NODE* node, AVL_ROOT* tree) {
 
 	if(node == 0) {
 #ifdef DEBUG
-		printf("%s:%s:null pointer passed to avl_rotate\n",__FILE__,__LINE__);
+		printf("%s:%s:null pointer passed to 
+avl_rotate\n",__FILE__,__LINE__);
 #endif
 		return -1;
 	}
